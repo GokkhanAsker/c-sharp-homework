0a33d1c1db06f23bf50e5571be56ac0249195dd1
GokkhanAsker <99753192+GokkhanAsker@users.noreply.github.com>
2022-02-25 23:24:03 +0300

    numbers min max

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите цифры 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите цифры 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (number2 > number1 )
+  {  
+    int max = number2;
+    int min = number1; 
+    Console.WriteLine("max: ");
+    Console.WriteLine( max);
+    Console.WriteLine("min: ");
+    Console.WriteLine(min);
+}
+else
+  {    
+    int max = number1;
+    int min = number2; 
+    Console.WriteLine("max: ");
+    Console.WriteLine( max);
+    Console.WriteLine("min: ");
+    Console.WriteLine(min);
+}   
+ 
+
