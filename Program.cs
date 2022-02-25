f360c81c6667dcc4f4c08914d213e5fc9d89e158
GokkhanAsker <99753192+GokkhanAsker@users.noreply.github.com>
2022-02-25 23:39:08 +0300

    143

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("Введите цифры 1: ");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите цифры 1: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите цифры 2: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
